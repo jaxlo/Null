600cc5908c7d7c64b0cf448dcbe140c41907c70b
Jordan Perry <jordan.hugh@yahoo.com>
2024-04-20 17:44:52 -0600

    Added extra questions + unlimited mass

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -22,7 +22,7 @@ function play(sound) {
 	})
 }
 
-var soundNames = ['pixelated-rush', 'q1', 'q2', 'q3', 'q4', 'ticktock', 'true', 'false']
+var soundNames = ['pixelated-rush', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'ticktock', 'true', 'false']
 
 var sounds = {}
 
